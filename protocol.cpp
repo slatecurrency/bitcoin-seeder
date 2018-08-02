6f3a4dc1e292fcb4c590e087a484805ea5262f0b
Gediminas <slatedeveloper@gmail.com>
2018-08-02 11:30:00 +1000

    Update seeder for SLATE

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xf9, 0xbe, 0xb4, 0xd9 };
+unsigned char pchMessageStart[4] = { 0xa3, 0xea, 0xb5, 0x81 };
 
 CMessageHeader::CMessageHeader()
 {
